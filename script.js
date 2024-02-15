15b2b0a2d82dd5348b7aab3e69290a3d5dd58427
kod-python <kelvinlik0245@gmail.com>
2024-02-15 10:15:52 +0000

    roload reload

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
 // write a program to display "i oove java script 5 times"
 
 
-for (i =1; i <=5; i++){
-    console.log(`i love javascript ${i}`)
+// for (i =1; i <=5; i++){
+//     console.log(`i love javascript ${i}`)
+// }
+
+
+for (let i =1; i<=5; i++){
+
+    console.log(`${i}`)
 }
